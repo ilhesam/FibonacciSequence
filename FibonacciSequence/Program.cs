5a1c639e64d90c6c66ee601fe57b56c795b8c192
Hesam Asnaashari <ilhesam@outlook.com>
2020-03-08 22:06:12 +0330

    Add project files.

---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        // Implementation Fibonacci Sequence in C# (.NET Core)
+
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            Console.Write("Please Enter Number for Fibonacci Calculation : ");
+            int number = int.Parse(Console.ReadLine());
+            
+            var result = FibonacciCalculator(number);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+
+        private static int FibonacciCalculator(int number)
+        {
+            if (number <= 1)
+            {
+                return number;
+            }
+
+            return FibonacciCalculator(number - 1) + FibonacciCalculator(number - 2);
+        }
+    }
+}
